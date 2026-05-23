e01a2a6f2656041f256ea7cd441a82c0c501957e
Anurag Rawat <148265902+AnuragRawat92@users.noreply.github.com>
2026-05-23 20:52:15 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,7 +14,10 @@ const ecoRoutes = require('./routes/eco');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://final-year-project-ecocreds-frontend.onrender.com",
+  credentials: true
+}));
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
